5f4c2e546c9359d493cadcb7fe39f20fc948724a
Miles <596442958@qq.com>
2016-03-27 15:06:15 +0800

    Initial Commit

---
+++ 丝袜美图/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  丝袜美图
+//
+//  Created by Miles on 3/27/16.
+//  Copyright © 2016 Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 丝袜美图/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  丝袜美图
+//
+//  Created by Miles on 3/27/16.
+//  Copyright © 2016 Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
